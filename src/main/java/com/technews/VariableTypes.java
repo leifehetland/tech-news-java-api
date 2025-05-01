5114291461987c7285094f117175742294a294b2
leifehetland <lehetland1@gmail.com>
2025-05-01 14:16:57 -0400

    variables and class practice

---
+++ src/main/java/com/technews/VariableTypes.java
@@ -0,0 +1,10 @@
+package com.technews;
+
+public class VariableTypes {
+    public static  void main(String[] args) {
+        String name = "Cleopatra";
+        int age = 2016 + 70;
+        double price = 1999.99;
+        boolean truth = true;
+    }
+}
---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,14 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args){
+        int num1 = 10;
+        double num2 = 20.5;
+        double sum = num1 + num2;
+        System.out.println(sum);
+
+        String banana = "banana" + num1;
+
+        System.out.println(banana);
+    }
+}
